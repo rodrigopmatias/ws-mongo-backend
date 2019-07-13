72571901938de0b5b9a2321d73f75d72a3a387ec
Rodrigo Pinheiro Matias <rodrigopmatias@gmail.com>
2019-07-13 02:38:01 -0300

    foram realizadas adequações

--- src/conf/tls.js
+++ src/conf/tls.js
@@ -1,14 +1,12 @@
 import fs from 'fs';
 
-const conf = {};
+const conf = { tls: false };
 
 if ((process.env.APP_TLS || '0') === '1') {
   conf.tls = true;
   conf.tlsCert = fs.readFileSync(process.env.APP_TLS_CERT || '/run/secrets/cert');
   conf.tlsKey = fs.readFileSync(process.env.APP_TLS_KEY || '/run/secrets/pkey');
   conf.tlsPort = Number.parseInt(process.env.APP_TLS_PORT || 8443, 10);
-} else {
-  conf.tls = false;
 }
 
 export default conf;
